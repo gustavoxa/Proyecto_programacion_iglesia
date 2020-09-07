910ec2ca5acbfb4f85cca243ed222a3baf310aa9
xavicoat <58531624+xavicoat@users.noreply.github.com>
2020-09-07 18:29:11 -0500

    Create Taller.cs

---
+++ Iglesiaform/Capadenegocios/Taller.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CapaDatos;
+
+namespace negocios
+{
+    public class Taller
+    {
+        Class1 basedatos = new Class1();
+        public void guardar(string _nombre)
+        {
+            List<ParametrosDB> lista = new List<ParametrosDB>();
+            lista.Add(new ParametrosDB("@descripcion", _nombre));
+            basedatos.EjecutarProcedimiento("Ingreso_Taller", lista);
+        }
+
+    }
+}
