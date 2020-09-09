72062360b427609bfbd57fe15fe352180d59dcf9
LORENA-PROYECT-HUB <68978423+LORENA-PROYECT-HUB@users.noreply.github.com>
2020-09-09 08:58:14 -0500

    Update SingletonTaller.cs

--- Iglesiaform/capanegocios/SingletonTaller.cs
+++ Iglesiaform/capanegocios/SingletonTaller.cs
@@ -6,7 +6,31 @@ using System.Threading.Tasks;
 
 namespace capanegocios
 {
-	class Singleton
+	class SingletonTaller
 	{
-	}
+        public sealed class Singleton
+        {
+            private static Singleton instance = null;
+            private static readonly object padlock = new object();
+
+            Singleton()
+            {
+            }
+
+            public static Singleton Instance
+            {
+                get
+                {
+                    lock (padlock)
+                    {
+                        if (instance == null)
+                        {
+                            instance = new Singleton();
+                        }
+                        return instance;
+                    }
+                }
+            }
+        }
+    }
 }
