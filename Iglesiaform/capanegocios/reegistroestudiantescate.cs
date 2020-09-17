efe7c4298270d3dc2444bc55691394b2b2f6a0a5
xavicoat <58531624+xavicoat@users.noreply.github.com>
2020-09-17 14:59:46 -0500

    Create reegistroestudiantescate.cs

---
+++ Iglesiaform/capanegocios/reegistroestudiantescate.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CapaDatos;
+using System.Data;
+namespace capanegocios
+{
+    class reegistroestudiantescate
+    {
+        private string V = "consultarepre";
+        DataSet datos;
+        public void ultimoregistro() {
+            Class1 busqueda = new Class1();
+            datos = busqueda.ultimoregistro(V);
+        }
+
+           
+    }
+}
